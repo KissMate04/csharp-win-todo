09b9000214352ab57b2f26cc4bd1be5ac4cc4942
KissMate04 <km.kissmate@gmail.com>
2025-07-04 10:27:33 +0200

    Add project files.

---
+++ todo/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace todo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbItem = new System.Windows.Forms.TextBox();
+            this.tbDate = new System.Windows.Forms.TextBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbItem
+            // 
+            this.tbItem.Location = new System.Drawing.Point(12, 41);
+            this.tbItem.Name = "tbItem";
+            this.tbItem.Size = new System.Drawing.Size(100, 20);
+            this.tbItem.TabIndex = 0;
+            this.tbItem.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbItem_KeyPress);
+            // 
+            // tbDate
+            // 
+            this.tbDate.Location = new System.Drawing.Point(139, 41);
+            this.tbDate.Name = "tbDate";
+            this.tbDate.Size = new System.Drawing.Size(100, 20);
+            this.tbDate.TabIndex = 1;
+            this.tbDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbDate_KeyPress);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(259, 44);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(86, 19);
+            this.checkBox.TabIndex = 2;
+            this.checkBox.Text = "checkBox1";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "fontos?";
+            // 
+            // listBox
+            // 
+            this.listBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox.ForeColor = System.Drawing.Color.Black;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(12, 81);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(325, 284);
+            this.listBox.TabIndex = 5;
+            this.listBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox_KeyPress);
+            // 
+            // btnDel
+            // 
+            this.btnDel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnDel.Location = new System.Drawing.Point(214, 402);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 4;
+            this.btnDel.Text = "Töröl";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAdd.Location = new System.Drawing.Point(59, 402);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Hozzáad";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(349, 450);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.tbDate);
+            this.Controls.Add(this.tbItem);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.3F);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(235)))), ((int)(((byte)(235)))));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(1500, 500);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Todo";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbItem;
+        private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ todo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace todo
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter sw;
+        StreamReader sr;
+        List<string> todoList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListUpdate()
+        {
+            
+            sr = new StreamReader(@"..\..\..\todo.txt");
+            while (!sr.EndOfStream) listBox.Items.Add(sr.ReadLine());
+            sr.Close();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            ListUpdate();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Add();
+        }
+
+        private void Add() {
+            if (tbItem.Text != "")
+            {
+                string date = "", priority = "";
+                if (tbDate.Text != "") date = tbDate.Text;
+                if (checkBox.Checked) priority = "!";
+
+                sw = new StreamWriter(@"..\..\..\todo.txt", true);
+                sw.WriteLine($"{tbItem.Text}\t{date}\t{priority}");
+                sw.Close();
+                listBox.Items.Clear();
+                ListUpdate();
+                tbItem.Clear();
+                tbDate.Clear();
+            }
+            tbItem.Focus();
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            Del();
+        }
+
+        private void Del() {
+            int i = listBox.SelectedIndex;
+            if (i >= 0)
+            {
+                todoList.Clear();
+                sr = new StreamReader(@"..\..\..\todo.txt");
+                while (!sr.EndOfStream) { todoList.Add(sr.ReadLine()); }
+                sr.Close();
+                todoList.RemoveAt(i);
+                sw = new StreamWriter(@"..\..\..\todo.txt");
+                foreach (var item in todoList)
+                {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+                listBox.Items.Clear();
+                ListUpdate();
+            }
+        }
+
+        private void tbItem_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)) Add();
+        }
+
+        private void tbDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)) Add();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)) Add();
+        }
+
+        private void listBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Back)) Del();
+        }
+    }
+}
